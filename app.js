559e72abc32c4873edb1215e2d97e16b77b387fa
sas8254 <shashibhushansingh11@gmail.com>
2022-12-20 13:49:26 +0530

    414. Campground Show.

--- app.js
+++ app.js
@@ -25,6 +25,10 @@ app.get("/campgrounds", async (req, res) => {
   const campgrounds = await Campground.find({});
   res.render("campgrounds/index", { campgrounds });
 });
+app.get("/campgrounds/:id", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/show", { campground });
+});
 
 app.listen(3000, () => {
   console.log("Serving on port 3000!");
