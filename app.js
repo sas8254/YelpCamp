fbac71b4cc8bd787061e2c4438aea48c538a48b2
sas8254 <shashibhushansingh11@gmail.com>
2022-12-23 15:42:15 +0530

    426. A New EJS Tool For Layouts.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 const mongoose = require("mongoose");
 const Campground = require("./models/campground");
@@ -15,8 +16,10 @@ mongoose
     console.log("mongo error", e);
   });
 
+app.engine("ejs", ejsMate); 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
