4ef48beeb1a6a31aa3c84ed82623fbb3470b441b
sas8254 <shashibhushansingh11@gmail.com>
2023-01-02 16:29:35 +0530

    495. Flash Errors Partial.

--- routes/campground.js
+++ routes/campground.js
@@ -44,6 +44,10 @@ router.get(
     const campground = await Campground.findById(req.params.id).populate(
       "reviews"
     );
+    if (!campground) {
+      req.flash("error", "Campground not found!");
+      return res.redirect("/campgrounds");
+    }
     res.render("campgrounds/show", { campground });
   })
 );
@@ -52,6 +56,10 @@ router.get(
   "/:id/edit",
   catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
+    if (!campground) {
+      req.flash("error", "Campground not found!");
+      return res.redirect("/campgrounds");
+    }
     res.render("campgrounds/edit", { campground });
   })
 );
