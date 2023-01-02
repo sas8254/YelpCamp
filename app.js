0d5c2b7de2062fc89e3fc4cd94d7e4db22b58fff
sas8254 <shashibhushansingh11@gmail.com>
2023-01-02 14:27:39 +0530

    491. Serving Static Assets.

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/reviews", reviewRoutes);
