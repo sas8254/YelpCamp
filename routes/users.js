5f70993513525c1c96c5d98b723845d38412c507
shashibhushan <shashibhushansingh11@gmail.com>
2023-01-03 09:07:06 +0530

    513. Register Route Logic.

--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,17 @@ router.get("/register", (req, res) => {
   res.render("users/register");
 });
 
-router.post("/register", (req, res) => {
-  res.send(req.body);
+router.post("/register", async (req, res) => {
+  try {
+    const { username, password, email } = req.body;
+    const user = new User({ username, email });
+    const registeredUser = await User.register(user, password);
+    req.flash("success", "Welcome to Yelp Camp!");
+    res.redirect("/campgrounds");
+  } catch (e) {
+    req.flash("error", e.message);
+    res.redirect("/register");
+  }
 });
 
 module.exports = router;
