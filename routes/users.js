d70d4d5f2d60f886459667aeb34ee341d24a834e
sas8254 <shashibhushansingh11@gmail.com>
2023-01-03 14:48:17 +0530

    514. Login Routes.

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/users");
+const passport = require("passport");
 
 router.get("/register", (req, res) => {
   res.render("users/register");
@@ -18,5 +19,21 @@ router.post("/register", async (req, res) => {
     res.redirect("/register");
   }
 });
+
+router.get("/login", (req, res) => {
+  res.render("users/login");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureFlash: true,
+    failureRedirect: "/login",
+  }),
+  (req, res) => {
+    req.flash("success", "Welcome back!");
+    res.redirect("/campgrounds");
+  }
+);
 
 module.exports = router;
