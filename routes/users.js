e171f8bc0dfd017b44355beece32738905e79556
sas8254 <shashibhushansingh11@gmail.com>
2023-01-03 16:38:23 +0530

    518. Fixing Register Route.

--- routes/users.js
+++ routes/users.js
@@ -2,19 +2,23 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/users");
 const passport = require("passport");
-const { reviewSchema } = require("../schemas");
 
 router.get("/register", (req, res) => {
   res.render("users/register");
 });
 
-router.post("/register", async (req, res) => {
+router.post("/register", async (req, res, next) => {
   try {
     const { username, password, email } = req.body;
     const user = new User({ username, email });
     const registeredUser = await User.register(user, password);
-    req.flash("success", "Welcome to Yelp Camp!");
-    res.redirect("/campgrounds");
+    req.login(registeredUser, (err) => {
+      if (err) {
+        return next(err);
+      }
+      req.flash("success", "Welcome to Yelp Camp!");
+      res.redirect("/campgrounds");
+    });
   } catch (e) {
     req.flash("error", e.message);
     res.redirect("/register");
