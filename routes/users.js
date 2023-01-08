1404360bd62160604320170eb49d4344edbbf3d1
shashibhushan <shashibhushansingh11@gmail.com>
2023-01-08 12:40:08 +0530

    intermediate

--- routes/users.js
+++ routes/users.js
@@ -1,57 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/users");
+const users = require("../controllers/users");
 const passport = require("passport");
 
-router.get("/register", (req, res) => {
-  res.render("users/register");
-});
+router.get("/register", users.renderRegister);
 
-router.post("/register", async (req, res, next) => {
-  try {
-    const { username, password, email } = req.body;
-    const user = new User({ username, email });
-    const registeredUser = await User.register(user, password);
-    req.login(registeredUser, (err) => {
-      if (err) {
-        return next(err);
-      }
-      req.flash("success", "Welcome to Yelp Camp!");
-      res.redirect("/campgrounds");
-    });
-  } catch (e) {
-    req.flash("error", e.message);
-    res.redirect("/register");
-  }
-});
+router.post("/register", users.register);
 
-router.get("/login", (req, res) => {
-  res.render("users/login");
-});
+router.get("/login", users.renderLogin);
 
-router.post(
-  "/login",
-  passport.authenticate("local", {
-    failureFlash: true,
-    failureRedirect: "/login",
-    keepSessionInfo: true,
-  }),
-  (req, res) => {
-    req.flash("success", "Welcome back!");
-    const redirectUrl = req.session.returnTo || "/campgrounds";
-    res.redirect(redirectUrl);
-    delete req.session.returnTo;
-  }
-);
+router.post("/login", users.login);
 
-router.get("/logout", (req, res) => {
-  req.logOut((err) => {
-    if (err) {
-      return next(err);
-    }
-    req.flash("success", "Goodbye!");
-    res.redirect("/campgrounds");
-  });
-});
+router.get("/logout", users.logout);
 
 module.exports = router;
---
+++ seeds/indexDesktop.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const Campground = require("../models/campground");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
+mongoose.set("strictQuery", false);
+mongoose
+  .connect("mongodb://127.0.0.1:27017/yelpcamp_v1")
+  .then(() => {
+    console.log("mongo connection open");
+  })
+  .catch((e) => {
+    console.log("mongo error", e);
+  });
+
+const sample = (array) => {
+  return array[Math.floor(Math.random() * array.length)];
+};
+
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const camp = new Campground({
+      author: "63b30714bc65da292b7b6389",
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/483251",
+      price: Math.floor(Math.random() * 20 + 10),
+      description:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio ducimus soluta ullam assumenda. Porro tempore adipisci eius fuga commodi sint autem deleniti? Rem excepturi vel a modi optio esse accusamus.",
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+    });
+    await camp.save();
+  }
+};
+
+seedDB().then(() => {
+  mongoose.connection.close();
+});
