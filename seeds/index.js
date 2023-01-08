f13a9fe3822e52710d42f6c3a6c60477c81a2eeb
sas8254 <shashibhushansingh11@gmail.com>
2023-01-08 21:34:13 +0530

    538. Fixing Our Seeds.

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Campground = require("../models/campground");
 const cities = require("./cities");
-const { places, descriptors } = require("./seedHelpers");
+const { places, descriptors, images } = require("./seedHelpers");
 mongoose.set("strictQuery", false);
 mongoose
   .connect("mongodb://127.0.0.1:27017/yelpcamp_v1")
@@ -20,10 +20,11 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const image = sample(images);
     const camp = new Campground({
       author: "63b3f257c5f19c7d90c3d1ef",
       title: `${sample(descriptors)} ${sample(places)}`,
-      image: "https://source.unsplash.com/collection/483251",
+      images: [{ url: image.url, filename: image.filename }],
       price: Math.floor(Math.random() * 20 + 10),
       description:
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio ducimus soluta ullam assumenda. Porro tempore adipisci eius fuga commodi sint autem deleniti? Rem excepturi vel a modi optio esse accusamus.",
