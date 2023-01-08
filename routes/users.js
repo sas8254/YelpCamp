578ffd4f80d934d4f0401366973811daa39fe90e
sas8254 <shashibhushansingh11@gmail.com>
2023-01-08 15:18:12 +0530

    528. A Fancy Way To Restructure Routes.

--- routes/users.js
+++ routes/users.js
@@ -3,21 +3,20 @@ const router = express.Router();
 const users = require("../controllers/users");
 const passport = require("passport");
 
-router.get("/register", users.renderRegister);
+router.route("/register")
+  .get(users.renderRegister)
+  .post(users.register);
 
-router.post("/register", users.register);
-
-router.get("/login", users.renderLogin);
-
-router.post(
-  "/login",
-  passport.authenticate("local", {
-    failureFlash: true,
-    failureRedirect: "/login",
-    keepSessionInfo: true,
-  }),
-  users.login
-);
+router.route("/login")
+  .get(users.renderLogin)
+  .post(passport.authenticate(
+    "local", {
+      failureFlash: true,
+      failureRedirect: "/login",
+      keepSessionInfo: true,
+    }),
+    users.login
+  );
 
 router.get("/logout", users.logout);
 
