8cd1b4273465ff6f795a44e7b97fb87e03b455f3
sas8254 <shashibhushansingh11@gmail.com>
2023-01-10 11:50:17 +0530

    546. Geocoding Our Locations.

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,5 +1,8 @@
 const Campground = require("../models/campground");
 const { cloudinary } = require("../cloudinary/index");
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 
 module.exports.index = async (req, res) => {
   const campgrounds = await Campground.find({});
@@ -11,14 +14,21 @@ module.exports.renderNewForm = (req, res) => {
 };
 
 module.exports.createCampground = async (req, res) => {
-  const campground = new Campground(req.body.campground);
-  campground.author = req.user._id;
-  campground.images = req.files.map((f) => {
-    return { url: f.path, filename: f.filename };
-  });
-  await campground.save();
-  req.flash("success", "Successfully made a Campground.");
-  res.redirect(`/campgrounds/${campground._id}`);
+  const geoData = await geocoder
+    .forwardGeocode({
+      query: req.body.campground.location,
+      limit: 1,
+    })
+    .send();
+  res.send(geoData.body.features[0].geometry.coordinates);
+  // const campground = new Campground(req.body.campground);
+  // campground.author = req.user._id;
+  // campground.images = req.files.map((f) => {
+  //   return { url: f.path, filename: f.filename };
+  // });
+  // await campground.save();
+  // req.flash("success", "Successfully made a Campground.");
+  // res.redirect(`/campgrounds/${campground._id}`);
 };
 
 module.exports.showCampground = async (req, res) => {
