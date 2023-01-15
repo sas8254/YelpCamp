df02cd6f0fbcc123e8c6a41be8eddcf668b7af6b
shashibhushan <shashibhushansingh11@gmail.com>
2023-01-15 21:23:33 +0530

    569. Minor Changes to Session/Cookies.

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(mongoSanitize());
 
 const sessionConfig = {
+  name: "session",
   secret: "theSecret",
   resave: false,
   saveUninitialized: true,
