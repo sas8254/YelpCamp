30d0609e90fe6cc3d0dac3784e2694eb73eff685
shashibhushan <shashibhushansingh11@gmail.com>
2023-01-15 12:17:47 +0530

    568. Sanitizing HTML w/ JOI.

--- schemas.js
+++ schemas.js
@@ -1,19 +1,43 @@
-const Joi = require("joi");
+const BaseJoi = require("joi");
+const sanitizeHtml = require("sanitize-html");
+
+const extension = (joi) => ({
+  type: "string",
+  base: joi.string(),
+  messages: {
+    "string.escapeHTML": "{{#label}} must not include HTML!",
+  },
+  rules: {
+    escapeHTML: {
+      validate(value, helpers) {
+        const clean = sanitizeHtml(value, {
+          allowedTags: [],
+          allowedAttributes: {},
+        });
+        if (clean !== value)
+          return helpers.error("string.escapeHTML", { value });
+        return clean;
+      },
+    },
+  },
+});
+
+const Joi = BaseJoi.extend(extension);
 
 module.exports.campgroundSchema = Joi.object({
   campground: Joi.object({
-    title: Joi.string().required(),
+    title: Joi.string().escapeHTML().required(),
     price: Joi.number().required().min(0),
-    // images: Joi.string().required(),
-    description: Joi.string().required(),
-    location: Joi.string().required(),
+    // image: Joi.string().required(),
+    location: Joi.string().escapeHTML().required(),
+    description: Joi.string().escapeHTML().required(),
   }).required(),
   deleteImages: Joi.array(),
 });
 
 module.exports.reviewSchema = Joi.object({
   review: Joi.object({
-    body: Joi.string().required(),
     rating: Joi.number().required().min(0).max(5),
+    body: Joi.string().escapeHTML().required(),
   }).required(),
 });
--- seeds/index.js
+++ seeds/index.js
@@ -21,6 +21,9 @@ const seedDB = async () => {
   for (let i = 0; i < 250; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const image = sample(images);
+    // user_id = "63b3f257c5f19c7d90c3d1ef" // laptop
+    // user_id = "63b30714bc65da292b7b6389" // Desktop
+
     const camp = new Campground({
       author: "63b3f257c5f19c7d90c3d1ef",
       title: `${sample(descriptors)} ${sample(places)}`,
